93354e10d246f8d522079a43b02d94a1fe84422b
Dinesh <dacha1@unh.newhaven.edu>
2020-11-20 19:17:20 -0500

    adding admin

--- music_webapp/settings.py
+++ music_webapp/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'music_webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'musicapp',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+
     }
 }
+
 
 
 # Password validation
